0d4febb90ad2108dd50f6040dfc100b396cb73ea
skt developer <shut7720@naver.com>
2023-06-15 13:57:49 +0900

    first 커밋

---
+++ src/main/java/com/example/pro_domain/ProDomainApplication.java
@@ -0,0 +1,13 @@
+package com.example.pro_domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProDomainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProDomainApplication.class, args);
+    }
+
+}
